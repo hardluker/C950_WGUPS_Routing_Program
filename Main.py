abac7dc19d2b9fe1cf001fe323c8c387c5ab5f6b
Donald Azevedo <dba9514@gmail.com>
2024-04-17 15:26:04 -0400

    Added the .csv files

--- Main.py
+++ Main.py
@@ -1,1 +1,11 @@
-#test
+import csv
+
+#Reading the Packages from the .csv
+with open("packages.csv") as package_file:
+    reader = csv.reader(package_file)
+    print(list(reader))
+
+#Reading the Distances from the .csv
+with open("distances.csv") as package_file:
+    reader = csv.reader(package_file)
+    print(list(reader))
