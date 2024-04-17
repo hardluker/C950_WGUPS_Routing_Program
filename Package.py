2e1e8c489450425314be37782e1390c81bea1139
hardluker <dba9514@gmail.com>
2024-04-17 18:22:39 -0400

    Created a Package class

---
+++ Package.py
@@ -0,0 +1,21 @@
+# Class for Packages
+class Package:
+
+
+    #Constructor initializing variables
+    def __init__(self, ID, address, city, state, zip, delivery_deadline, weight, status):
+        self.ID = ID
+        self.address = address
+        self.city = city
+        self.state = state
+        self.zip = zip
+        self.delivery_deadline = delivery_deadline
+        self.weight = weight
+        self.status = status
+
+
+    #Method for converting object to string
+    def __str__(self):
+        return "%s, %s, %s, %s, %s, %s, %s, %s, %s" % (self.ID, self.address, self.city, self.state, self.zip,
+                                                       self.delivery_deadline, self.weight,
+                                                       self.status)
