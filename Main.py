0f2b747760897db7f2785afb6428177bdda40c7e
Donald Azevedo <dba9514@gmail.com>
2024-04-19 13:11:33 -0400

    Refactored the package manager It is no longer coupled to a specific directory but rather passed as arguments

--- Main.py
+++ Main.py
@@ -15,22 +15,3 @@ address2 = package_manager.get_package(3).address
 
 print(address_manager.get_address_differences(address1, address2))
 
-
-# Creating the HashTable and inserting the packages into it. The size is 52
-# hash_table = HashTable(52)
-# insert_packages_into_hash_table("CSV_Files/Packages.csv", hash_table)
-
-
-
-# address2 = hash_table.get(2).address
-# address1 = hash_table.get(30).address
-
-# address_manager = AddressManager()
-
-# difference = address_manager.get_address_differences(address1, address2)
-
-
-
-# print(f"The distance between {address1} and {address2} is: {difference}")
-# Instantiating and "Loading" the trucks with a list of the package IDs.
-# truck1 = Truck(16, 0, [], 18, 0.0, )
