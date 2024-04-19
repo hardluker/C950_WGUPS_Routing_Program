cc3ba5f4f481def53704769fdadf6576f028471f
Donald Azevedo <dba9514@gmail.com>
2024-04-19 12:41:14 -0400

    Established a PackageManager This serves as an interface to the packages and the hashmap data structure

--- Main.py
+++ Main.py
@@ -1,49 +1,29 @@
 import csv
 from HashTable import HashTable
-from Package import Package
+from PackageManager import PackageManager
 from AddressManager import AddressManager
 from Truck import Truck
 
-#Function for inserting the packages into the hash table
-def insert_packages_into_hash_table(file, HashTable):
-    with open(file) as package_file:
-        next(package_file) #Skipping the header in the csv file
-        packages = csv.reader(package_file)
+package_manager = PackageManager("CSV_Files/Packages.csv")
 
-        #Iterating through the items in the csv and defining them
-        for package in packages:
-            id = int(package[0])
-            address = package[1]
-            city = package[2]
-            state = package[3]
-            zip_code = package[4]
-            delivery_deadline = package[5]
-            weight = package[6]
-            status = "In Hub"
-
-            # Inputting the data into a package object
-            package_to_insert = Package(id, address, city, state, zip_code,
-                                        delivery_deadline, weight, status)
-            HashTable.insert(id, package_to_insert)
+print(package_manager.get_package(1).address)
 
 
-#Creating the HashTable and inserting the packages into it.
-hash_table = HashTable(30)
-insert_packages_into_hash_table("CSV_Files/Packages.csv", hash_table)
-
-
-#Get the distance between 2 addresses
-#def get_address_distances(address1, address2):
-
-address2 = hash_table.get(4).address
-address1 = hash_table.get(5).address
-
-address_manager = AddressManager()
-
-difference = address_manager.get_address_differences(address1, address2)
+#Creating the HashTable and inserting the packages into it. The size is 52
+#hash_table = HashTable(52)
+#insert_packages_into_hash_table("CSV_Files/Packages.csv", hash_table)
 
 
 
-print(f"The distance between {address1} and {address2} is: {difference}")
+#address2 = hash_table.get(2).address
+#address1 = hash_table.get(30).address
+
+#address_manager = AddressManager()
+
+#difference = address_manager.get_address_differences(address1, address2)
+
+
+
+#print(f"The distance between {address1} and {address2} is: {difference}")
 #Instantiating and "Loading" the trucks with a list of the package IDs.
 #truck1 = Truck(16, 0, [], 18, 0.0, )
---
+++ PackageManager.py
@@ -0,0 +1,38 @@
+import csv
+import math
+from HashTable import HashTable
+from Package import Package
+class PackageManager:
+
+    #Package Manager Constructor
+    #Reads the packages as a list, calculates a table size 1.3x the list.
+    #Then, creates hashtable and inserts them into the list
+    def __init__(self, package_directory):
+        self.packages = self.read_packages(package_directory)
+        table_size = int(math.ceil(len(self.packages) * 1.3))
+        self.hash_table = HashTable(table_size)
+        self.insert_packages_into_hash_table(self.packages, self.hash_table)
+
+    def read_packages(self, package_directory):
+        with open(package_directory) as package_file:
+            next(package_file)  # Skipping the header in the csv file
+            return list(csv.reader(package_file))
+
+    def insert_packages_into_hash_table(self, packages, hash_table):
+        for package in packages:
+            id = int(package[0])
+            address = package[1]
+            city = package[2]
+            state = package[3]
+            zip_code = package[4]
+            delivery_deadline = package[5]
+            weight = package[6]
+            status = "In Hub"
+
+            # Inputting the data into a package object
+            package_to_insert = Package(id, address, city, state, zip_code,
+                                        delivery_deadline, weight, status)
+            hash_table.insert(id, package_to_insert)
+
+    def get_package(self, package_id):
+        return self.hash_table.get(package_id)
